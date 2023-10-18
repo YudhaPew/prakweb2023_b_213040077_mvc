6783f2bc601ce7c3aea22904c79dabbc9a412eee
YudhaPew <yudhaprasetya678@gmail.com>
2023-10-18 18:05:08 +0700

    Controller MVC

---
+++ phpmvc/app/controllers/About.php
@@ -0,0 +1,13 @@
+<?php
+
+class About
+{
+  public function index($nama = 'Yudha Prasetya', $pekerjaan = 'Manusia')
+  {
+    echo "Halo, Nama saya $nama, Saya $pekerjaan ";
+  }
+  public function page()
+  {
+    echo 'About/page';
+  }
+}
