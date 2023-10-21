294d709870c9d4b35e1e27a3bb900e544b8816af
YudhaPew <yudhaprasetya678@gmail.com>
2023-10-21 22:53:52 +0700

    Database Wrapper MVC

---
+++ phpmvc/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://prakweb2023_b_213040077_mvc.test/phpmvc/public');
+
+// db
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'prakweb2023_b_213040077_mvc');
--- phpmvc/app/core/Constants.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-define('BASEURL', 'http://prakweb2023_b_213040077_mvc.test/phpmvc/public');
