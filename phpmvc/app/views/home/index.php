ea2ec8af658af9c4d9d6c80e7cb7a8300dcfee66
YudhaPew <yudhaprasetya678@gmail.com>
2023-10-22 22:24:36 +0700

    Searching MVC

--- phpmvc/app/views/home/index.php
+++ phpmvc/app/views/home/index.php
@@ -1,6 +1,6 @@
 <div class="container">
   <div class="bg-primary p-5 rounded-lg m-3">
-    <h1 class="display-4">Selamat Datang di Yudha Gaming </h1>
+    <h1 class="display-4">Selamat Datang di WEBSITE MVC </h1>
     <p class="lead">Halo nama saya <?= $data['nama']; ?></p>
     <hr class="my-4">
     <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
